80caf0751e800180f8f0f3610d2e73290d3769f3
Kurt Stephens <ks.github@kurtstephens.com>
2010-07-29 04:18:53 -0500

    Strings are constants, but should be cloneable.

--- src/ll/constant.c
+++ src/ll/constant.c
@@ -1,20 +1,4 @@
-
-#ifndef __rcs_id__
-#ifndef __rcs_id_ll_constant_c__
-#define __rcs_id_ll_constant_c__
-static const char __rcs_id_ll_constant_c[] = "$Id: constant.c,v 1.5 2007/09/08 23:51:19 stephens Exp $";
-#endif
-#endif /* __rcs_id__ */
-
 #include "ll.h"
-
-/**************************************************************************/
-
-ll_define_primitive(constant, clone, _1(object), _1(no_side_effect,"#t"))
-{
-  ll_return(ll_SELF);
-}
-ll_define_primitive_end
 
 /**************************************************************************/
 /* mutable/immutable */
