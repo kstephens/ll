e0fbef33c4d96a1c2a35047b9cc1520a9713afab
Kurt Stephens <ks.github@kurtstephens.com>
2011-06-16 22:46:57 -0500

    src/hash changes/fixes from smal/src/hash.

--- src/hash/voidP_voidP_Table.c
+++ src/hash/voidP_voidP_Table.c
@@ -5,11 +5,13 @@
 static
 unsigned int voidP_hash(void *_x)
 {
-  const unsigned char *x = (const char*) &_x;
+  const unsigned char *x = (const unsigned char*) &_x;
   unsigned int hash = x[0];
 
-#define DO_HASH(i) hash ^= (hash << 15) | (hash >> 1) ^ x[i]
+#define DO_HASH(i) hash ^= ((hash << 15) | (hash >> 1)) ^ x[i]
   switch ( sizeof(_x) ) {
+  default:
+    abort();
   case 8:
     DO_HASH(7);
   case 7:
@@ -24,8 +26,6 @@ unsigned int voidP_hash(void *_x)
     DO_HASH(2);
   case 2:
     DO_HASH(1);
-  default:
-    abort();
   }
 #undef DO_HASH
 
